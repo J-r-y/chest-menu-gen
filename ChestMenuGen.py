5b54542e79ee207f83d5b37ef4e120e130d886ee
Jerry <110570214+J-r-y@users.noreply.github.com>
2022-08-04 01:00:23 +0200

    Add files via upload

--- ChestMenuGen.py
+++ ChestMenuGen.py
@@ -1,38 +1,69 @@
 from PIL import Image
 
 rows = 6
+non_stackable = False
+non_stackable_items = ["_bucket",
+                       "boat",
+                       "sword",
+                       "pickaxe",
+                       "axe",
+                       "hoe",
+                       "shovel",
+                       "helmet",
+                       "chestplate",
+                       "leggings",
+                       "boots",
+                       "bow",
+                       "cake",
+                       "elytra",
+                       "flint_and_steel",
+                       "fishing_rod",
+                       "minecart",
+                       "music_disc",
+                       "writable_book",
+                       "trident"]
 
 print("Input: big / small (chest), item (der genaue name, statt unterstrich leertaste) z.B.: big, melon slice")
-eingabe = input()
-config = eingabe.split(", ")
-if " " in config[1]:
-    item_url = config[1].replace(" ", "_")
+eingabe = input()  # Input in 'eingabe' speichern
+configuration = eingabe.split(", ")  # Kisten-Typ und Item speichern
+if " " in configuration[1]:
+    item_url = configuration[1].replace(" ", "_")  # Leerzeichen mit Unterstrich ersetzen
 else:
-    item_url = config[1]
+    item_url = configuration[1]
 
 try:
-    item_img = Image.open(f"res/items/{item_url}.png")
+    item_img = Image.open(f"res/items/{item_url}.png")  # versuchen, das Bild des Items zu laden
     item_img = item_img.convert("RGBA")
 except FileNotFoundError:
-    print(f"{config[1]} ist kein item in minecraft.")
+    print(f"{configuration[1]} ist kein item in minecraft oder du hast dich verschrieben.")
     exit(1)
-if item_url == "ender_pearl":
-    count_img = Image.open("res/count16.png")
+
+if item_url == "ender_pearl" or "sign" in item_url or "egg" in item_url:
+    count_img = Image.open("res/count16.png")  # wenn ein Stack 16 ist
 else:
-    count_img = Image.open("res/count64.png")
+    count_img = Image.open("res/count64.png")  # wenn ein Stack 64 ist
 count_img = count_img.convert("RGBA")
 
-if config[0] == "big":
+# Kisten-Typ abgleichen und das passende Bild laden
+if configuration[0] == "big":
     rows = 6
     chest_img = Image.open("res/chest_big.png")
-elif config[0] == "small":
+elif configuration[0] == "small":
     rows = 3
     chest_img = Image.open("res/chest_small.png")
 chest_img = chest_img.convert("RGBA")
 
+# check ob das Item sich überhaupt stacken lässt (z.B. Rüstung kann man nicht stacken)
+for z in non_stackable_items:
+    if z in item_url:
+        non_stackable = True
+
+# die Item-Bilder und "16" bzw. "64" in jeden Slot des Kisten-Menüs einfügen
 for y in range(rows):
     for x in range(9):
         chest_img.paste(item_img, (x * 18 + 8, y * 18 + 8), item_img)
-        chest_img.paste(count_img, (x * 18 + 9, y * 18 + 9), count_img)
+        if not non_stackable:
+            chest_img.paste(count_img, (x * 18 + 8, y * 18 + 8), count_img)
 
-chest_img.save("output.png", quality=100)
+# das fertige Bild speichern
+chest_img.save("kiste.png", quality=100, format="png")
